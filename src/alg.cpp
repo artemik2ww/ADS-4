17a91156fae9a5088a834d9e3b507f992cfc24d4
Артём <71376415+artemik2ww@users.noreply.github.com>
2026-05-03 16:28:10 +0300

    b125
    b125

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,88 +1,110 @@
-// Copyright 2021 NNTU-CS
-#include <cstddef>
+static int lowerBoundInt(int* arr, int len, int x)
+{
+    int l = 0, r = len;
+    while (l < r) {
+        int m = l + (r - l) / 2;
+        if (arr[m] < x) l = m + 1;
+        else r = m;
+    }
+    return l;
+}
 
-int countPairs1(int *arr, int len, int value) {
-    int count = 0;
+static int upperBoundInt(int* arr, int len, int x)
+{
+    int l = 0, r = len;
+    while (l < r) {
+        int m = l + (r - l) / 2;
+        if (arr[m] <= x) l = m + 1;
+        else r = m;
+    }
+    return l;
+}
+
+int countPairs1(int* arr, int len, int value) {
+    int k = 0;
     for (int i = 0; i < len; i++) {
         for (int j = i + 1; j < len; j++) {
-            if (arr[i] + arr[j] == value) {
-                count++;
-            }
+            if (arr[i] + arr[j] == value)
+                k++;
         }
     }
-    return count;
+
+    return k;
 }
 
-int countPairs2(int *arr, int len, int value) {
-    int count = 0;
-    int left = 0;
-    int right = len - 1;
+int countPairs2(int* arr, int len, int value) {
+    int l = 0;
+    int r = len - 1;
+    int k = 0;
 
-    while (left < right) {
-        int sum = arr[left] + arr[right];
-        if (sum == value) {
-            if (arr[left] == arr[right]) {
-                int n = right - left + 1;
-                count += n * (n - 1) / 2;
+    while (l < r) {
+        int sum = arr[l] + arr[r];
+
+        if (sum < value) {
+            l++;
+        } else if (sum > value) {
+            r--;
+        } else {
+            if (arr[l] == arr[r]) {
+                int n = r - l + 1;
+                k += n * (n - 1) / 2;
                 break;
             }
-            int left_val = arr[left];
-            int left_count = 0;
-            while (left <= right && arr[left] == left_val) {
-                left_count++;
-                left++;
+
+            int leftValue = arr[l];
+            int rightValue = arr[r];
+
+            int lCount = 0;
+            int rCount = 0;
+
+            while (l <= r && arr[l] == leftValue) {
+                l++;
+                lCount++;
             }
-            int right_val = arr[right];
-            int right_count = 0;
-            while (right >= left && arr[right] == right_val) {
-                right_count++;
-                right--;
+
+            while (r >= l && arr[r] == rightValue) {
+                r--;
+                rCount++;
             }
-            count += left_count * right_count;
-        } else if (sum < value) {
-            left++;
-        } else {
-            right--;
+
+            k += lCount * rCount;
         }
     }
-    return count;
+
+    return k;
 }
 
-int binarySearch(int *arr, int left, int right, int target) {
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == target) {
-            return mid;
-        } else if (arr[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-    return -1;
-}
+int countPairs3(int* arr, int len, int value)
+{
+    int k = 0;
+    int i = 0;
 
-int countPairs3(int *arr, int len, int value) {
-    int count = 0;
-    for (int i = 0; i < len; i++) {
-        int target = value - arr[i];
-        int pos = binarySearch(arr, i + 1, len - 1, target);
-        if (pos != -1) {
-            int first = pos;
-            int last = pos;
-            
-            while (first > i + 1 && arr[first - 1] == target) {
-                first--;
-            }
-            
-            while (last < len - 1 && arr[last + 1] == target) {
-                last++;
-            }
-            count += (last - first + 1);
-        }
-        while (i + 1 < len && arr[i] == arr[i + 1]) {
+    while (i < len) {
+        int cur = arr[i];
+        int curs = i;
+
+        while (i < len && arr[i] == cur)
             i++;
+
+        int curk = i - curs;
+        int need = value - cur;
+
+        if (need < cur)
+            continue;
+
+        int lb = lowerBoundInt(arr, len, need);
+        int ub = upperBoundInt(arr, len, need);
+        int needk = ub - lb;
+
+        if (needk == 0)
+            continue;
+
+        if (need == cur) {
+            k += curk * (curk - 1) / 2;
+        } else {
+            k += curk * needk;
         }
     }
-    return count;
+
+    return k;
 }
